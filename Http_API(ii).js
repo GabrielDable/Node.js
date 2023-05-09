f2d30761698600010cf63733c1ef193e3b1da8c5
Martins Castro <110532656+GabrielDable@users.noreply.github.com>
2023-05-09 10:12:00 +0100

    Create Http_API(ii).js

---
+++ Http_API(ii).js
@@ -0,0 +1,75 @@
+
+The Http API Prorotype Chain 
+
+
+
+(A) 
+
+
+(i) - http.prototype.Server.
+
+
+    Http.Prototype.createServer(options, RequestHandler) {returns http. Server} -//  Http.server instanceof Net.prototype.Server && Http.prototype && EventEmitter.prototype.
+
+            Http.server.on("request",  function RequestHandler (Http.incomingMessage, Http.serverResponse) => {}. // Http.Server is EventEmitter and Net.Server an listen to a request event
+                       
+                       
+(ii) - http.prototype.ClientRequest.
+
+
+    Http.request(options, ResponseHandler(Http.prototype.IncomingMessage) {return Http.Prototype.OutgoingMessage.Prottype.ClientRequest}; // Http.ClientRequest instanceof Stream.prototype.Writable && Http.OutgoingMessage
+
+
+
+(iii) http.prototype.ServerResponse. 
+
+  http.prototype.ServerResponse instanceof stream.prototype.writable
+      Http.Server.on("request", (http.prototype.IncomingMessage, https.prototype.OutgoingMessage.ServerResonse => {});
+
+
+(iv) Https.prototype.OutgoingMessage.
+
+
+    It is the prototype of Http.ClientRequest and Http.Prototype.ServerReponse and extends to Stream.prototype.
+
+
+                       
+(v) http.prototype.IncomingMessage.
+
+
+    http.prototype.IncomingMessage instanceof stream.prototype.readable
+
+    It is generated in  events within Https Apis
+
+      Http.Server.on("request", (http.prototype.IncomingMessage, https.prototype.OutgoingMessage.ServerResonse => {});
+       Http.ClientRequest.on("response", ResponseHandler(Httt.Prorotype.OugoingMessage.Prototype.IncomingMessage);
+
+
+(B) METHODS and Events OF Stream.prototype.writable.https.prototype.IncomingMessage.
+
+       Methods
+
+      IncomingMessage.complete typeOf Boolean // If the message was fully received and parsed
+      -----------------.statuscode. 
+      -----------------.headers.
+      -----------------.methods.
+
+      
+      Events
+
+      
+
+(c)
+
+
+
+ 
+      
+                 
+
+
+
+               
+               
+
+
